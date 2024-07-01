afe07e7ec55f0c84413e35884eff9334afb768ee
jacksonkasi1 <jacksonkasipeacock@gmail.com>
2024-07-01 16:45:10 +0530

    added clerk auth in forntend

--- next-graphlq-clerk-drizzle-all-in-one/src/app/layout.tsx
+++ next-graphlq-clerk-drizzle-all-in-one/src/app/layout.tsx
@@ -2,6 +2,14 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
+import {
+  ClerkProvider,
+  SignInButton,
+  SignedIn,
+  SignedOut,
+  UserButton
+} from '@clerk/nextjs'
+
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -15,8 +23,24 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
+    <ClerkProvider
+      appearance={{
+        variables: { colorPrimary: "#4E11C7" },
+      }}
+    >
+      <html lang="en">
+        <body className={inter.className}>
+          <div className="w-full mx-auto p-10 top-5">
+            <SignedOut>
+              <SignInButton />
+            </SignedOut>
+            <SignedIn>
+              <UserButton />
+            </SignedIn>
+          </div>
+          {children}
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
