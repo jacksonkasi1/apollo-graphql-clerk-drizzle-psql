7482edf4a159e0e3a7e49ffc62d0dbf52b0ea5ad
jacksonkasi1 <jacksonkasipeacock@gmail.com>
2024-07-04 22:29:43 +0530

    chore: Update next.config.mjs to include Access-Control-Allow headers for /api/graphql endpoint

--- next-graphlq-clerk-drizzle-all-in-one/next.config.mjs
+++ next-graphlq-clerk-drizzle-all-in-one/next.config.mjs
@@ -1,4 +1,26 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    async headers() {
+      return [
+        {
+          source: '/api/graphql',
+          headers: [
+            {
+              key: 'Access-Control-Allow-Origin',
+              value: '*',
+            },
+            {
+              key: 'Access-Control-Allow-Methods',
+              value: 'GET, POST, OPTIONS',
+            },
+            {
+              key: 'Access-Control-Allow-Headers',
+              value: 'Content-Type, Authorization',
+            },
+          ],
+        },
+      ];
+    },
+  };
+  
+  export default nextConfig;
